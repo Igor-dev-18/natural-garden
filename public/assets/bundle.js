878b42f5e4750bb1b3313c4b071dfd1da413410f
Igor <igorblackrock@gmail.com>
2022-02-12 21:59:58 -0300

    Add JavaScript events

--- public/assets/bundle.js
+++ public/assets/bundle.js
@@ -61,9 +61,54 @@ var __webpack_exports__ = {};
   \*************************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../sass/main.scss */ "./src/sass/main.scss");
+ // alternador do menu
+// selecionando o botão toggleMenu
 
-var teste = 'oi';
-console.log(teste);
+var toggleMenu = document.querySelector('.header-toggle-menu'); // adicionar um evento de clique ao botão
+// ao clicarmos no botão será adicionado uma classe active
+// tanto para o botão como para o menu
+// a classe active do menu faz o menu abrir
+
+toggleMenu.addEventListener('click', function () {
+  // add active ao botão
+  this.classList.toggle('active'); // selecionando o menu próximo ao botão
+
+  var menu = this.nextElementSibling; // add active ao menu
+
+  menu.classList.toggle('active');
+}); // faqs
+
+var questions = document.querySelectorAll('.faq__header');
+questions.forEach(function (question) {
+  question.addEventListener('click', function () {
+    var itemAtivo = document.querySelector('.faq.active .faq__header');
+
+    if (itemAtivo && itemAtivo !== this) {
+      console.log('executei'); // fecha quem tá aberto
+
+      checkFaq(itemAtivo);
+    }
+
+    checkFaq(question);
+  });
+});
+
+function checkFaq(question) {
+  // seleciona o item pai
+  var parent = question.parentElement; // add active ao item pai
+
+  parent.classList.toggle('active'); // seleciona a resposta
+
+  var answer = question.nextElementSibling;
+
+  if (answer.style.maxHeight) {
+    // se tiver altura então "feche"
+    answer.style.maxHeight = null;
+  } else {
+    //senão abra
+    answer.style.maxHeight = answer.scrollHeight + 'px';
+  }
+}
 })();
 
 /******/ })()
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,66 @@
 import '../sass/main.scss';
 
+// alternador do menu
+// selecionando o botão toggleMenu
+const toggleMenu = document.querySelector('.header-toggle-menu');
 
-let teste = 'oi';
-console.log( teste);
+// adicionar um evento de clique ao botão
+// ao clicarmos no botão será adicionado uma classe active
+// tanto para o botão como para o menu
+// a classe active do menu faz o menu abrir
+toggleMenu.addEventListener('click', function () {
+
+    // add active ao botão
+    this.classList.toggle('active');
+    // selecionando o menu próximo ao botão
+    const menu = this.nextElementSibling;
+    // add active ao menu
+    menu.classList.toggle('active');
+});
+
+
+// faqs
+const questions = document.querySelectorAll('.faq__header');
+
+questions.forEach(question => {
+    question.addEventListener('click', function () {
+
+        let itemAtivo = document.querySelector('.faq.active .faq__header2');
+
+        if (itemAtivo && itemAtivo !== this) {
+            console.log('executei');
+            // fecha quem tá aberto
+            checkFaq(itemAtivo);
+        }
+        checkFaq(question);
+    });
+});
+
+function checkFaq(question) {
+
+    // seleciona o item pai
+    const parent = question.parentElement;
+
+    // add active ao item pai
+    parent.classList.toggle('active');
+
+    // seleciona a resposta
+    const answer = question.nextElementSibling;
+
+
+    if (answer.style.maxHeight) {
+        // se tiver altura então "feche"
+        answer.style.maxHeight = null;
+    }
+    else {
+        //senão abra
+        answer.style.maxHeight = answer.scrollHeight + 'px';
+    }
+}
+
+
+
+
+
+
 
